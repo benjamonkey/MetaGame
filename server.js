54b7d57baf010c1ea9359693a613dc849f3b2bee
Ben Hutchings <benhutchings@Devs-MacBook-Pro.local>
2016-07-01 17:27:57 -0600

    reorganized to use nodemon instead of live-server, many things broken as a result. Trying to use both the games and game search from the api, made another GET to the game url, searchbox is no longer working

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 var axios = require('axios');
 var cors = require('cors');
+app.use(express.static(__dirname + '/public'));
 app.use(cors());
 var data = [];
 
 app.listen(3000, function(){console.log("we on port 3000 dude")});
 
-app.get('/:searchTB', function(req, res){
+app.get('/games/:searchTB', function(req, res){
+  console.log("getting games by name");
   axios.get(`http://www.giantbomb.com/api/games/?api_key=53f52d0efe71c57da724633715458b37cd07a278&format=json&sort=original_release_date:desc&filter=name:${req.params.searchTB}`)
+   .then( resp => res.send(resp.data))
+})
+
+app.get('/game/:searchTB', function(req, res){
+  console.log("getting game by id");
+  axios.get(`http://www.giantbomb.com/api/game/?api_key=53f52d0efe71c57da724633715458b37cd07a278&format=json&sort=original_release_date:desc&filter=name:${req.params.searchTB}`)
    .then( resp => res.send(resp.data))
 })
