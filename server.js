3095b8bfe303efb9c53cb826ac4eba582639f072
Ben Hutchings <benhutchings@Devs-MacBook-Pro.local>
2016-07-01 11:11:02 -0600

    installed angular in node_modules rather than pinging the cdn link

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
@@ -10,6 +11,6 @@ var data = [];
 app.listen(3000, function(){console.log("we on port 3000 dude")});
 
 app.get('/:searchTB', function(req, res){
-  axios.get(`http://www.giantbomb.com/api/games/?api_key=53f52d0efe71c57da724633715458b37cd07a278&format=json&filter=name:${req.params.searchTB}`)
+  axios.get(`http://www.giantbomb.com/api/games/?api_key=53f52d0efe71c57da724633715458b37cd07a278&format=json&sort=original_release_date:desc&filter=name:${req.params.searchTB}`)
    .then( resp => res.send(resp.data))
 })
